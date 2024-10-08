a3e31412d7855d1f1b6bb40b987b5782e6fb1d32
afaslann <afapm33@gmail.com>
2024-10-08 19:20:26 +0300

    Add files via upload

---
+++ class2.py
@@ -0,0 +1,12 @@
+class Tenis():
+    turnuva = "wimbeldon"
+
+    def __init__(self, oyuncuadi, yas, galibiyet):
+        self.oyuncuadi = oyuncuadi
+        self.yas = yas
+        self.galibiyet = galibiyet
+karsilasma1 = Tenis('alcaraz', 21, 2 )
+print(karsilasma1.turnuva)
+print(karsilasma1.oyuncuadi)
+print(karsilasma1.yas)
+print(karsilasma1.galibiyet)
